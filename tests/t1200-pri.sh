825c60514f551dc6a2970ec01b873dd7e0cf234e
Emil Sit <sit@emilsit.net>
2009-04-13 20:54:36 -0400

    tests: Factor basic add/list, replace, priority testing out of t9999
    add/list and replace are already covered by existing tests.
    Add a new test, t1200-pri.sh, that handles the priority tests.
    
    Signed-off-by: Emil Sit <sit@emilsit.net>

---
+++ tests/t1200-pri.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+test_description='basic priority functionality
+'
+. ./test-lib.sh
+
+test_todo_session 'priority usage' <<EOF
+>>> todo.sh pri B B
+usage: $HOME/bin/todo.sh pri ITEM# PRIORITY
+note: PRIORITY must be anywhere from A to Z.
+=== 1
+EOF
+
+cat > todo.txt <<EOF
+smell the uppercase Roses +flowers @outside
+notice the sunflowers
+stop
+EOF
+test_todo_session 'basic priority' <<EOF
+>>> todo.sh list
+2 notice the sunflowers
+1 smell the uppercase Roses +flowers @outside
+3 stop
+--
+TODO: 3 of 3 tasks shown from $HOME/todo.txt
+
+>>> todo.sh pri 1 B
+1: (B) smell the uppercase Roses +flowers @outside
+TODO: 1 prioritized (B).
+
+>>> todo.sh list
+[0;32m1 (B) smell the uppercase Roses +flowers @outside[0m
+2 notice the sunflowers
+3 stop
+--
+TODO: 3 of 3 tasks shown from $HOME/todo.txt
+
+>>> todo.sh -p list
+1 (B) smell the uppercase Roses +flowers @outside
+2 notice the sunflowers
+3 stop
+--
+TODO: 3 of 3 tasks shown from $HOME/todo.txt
+
+>>> todo.sh pri 2 C
+2: (C) notice the sunflowers
+TODO: 2 prioritized (C).
+
+>>> todo.sh -p list
+1 (B) smell the uppercase Roses +flowers @outside
+2 (C) notice the sunflowers
+3 stop
+--
+TODO: 3 of 3 tasks shown from $HOME/todo.txt
+
+>>> todo.sh pri 2 A
+2: (A) notice the sunflowers
+TODO: 2 prioritized (A).
+
+>>> todo.sh -p list
+2 (A) notice the sunflowers
+1 (B) smell the uppercase Roses +flowers @outside
+3 stop
+--
+TODO: 3 of 3 tasks shown from $HOME/todo.txt
+
+>>> todo.sh pri 2 a
+2: (A) notice the sunflowers
+TODO: 2 prioritized (A).
+
+>>> todo.sh -p listpri
+2 (A) notice the sunflowers
+1 (B) smell the uppercase Roses +flowers @outside
+--
+TODO: 2 of 3 tasks shown from $HOME/todo.txt
+
+>>> todo.sh add "smell the coffee +wakeup"
+TODO: 'smell the coffee +wakeup' added on line 4.
+
+>>> todo.sh -p list
+2 (A) notice the sunflowers
+1 (B) smell the uppercase Roses +flowers @outside
+4 smell the coffee +wakeup
+3 stop
+--
+TODO: 4 of 4 tasks shown from $HOME/todo.txt
+EOF
+
+test_done
--- tests/t9999-testsuite_example.sh
+++ tests/t9999-testsuite_example.sh
@@ -4,140 +4,13 @@ test_description='basic tests imported from previous framework
 '
 . ./test-lib.sh
 
+cat > todo.txt <<EOF
+(B) smell the uppercase Roses +flowers @outside
+(A) notice the sunflowers
+stop
+smell the coffee +wakeup
+EOF
 test_todo_session 'basic tests' <<EOF
->>> todo.sh add "notice the daisies"
-TODO: 'notice the daisies' added on line 1.
-
->>> todo.sh list
-1 notice the daisies
---
-TODO: 1 of 1 tasks shown from $HOME/todo.txt
-
->>> todo.sh replace adf asdfa
-usage: $HOME/bin/todo.sh replace ITEM# "UPDATED ITEM"
-=== 1
-
->>> todo.sh replace 1 "smell the cows"
-1: notice the daisies
-replaced with
-1: smell the cows
-
->>> todo.sh list
-1 smell the cows
---
-TODO: 1 of 1 tasks shown from $HOME/todo.txt
-
->>> todo.sh replace 1 smell the roses
-1: smell the cows
-replaced with
-1: smell the roses
-
->>> todo.sh list
-1 smell the roses
---
-TODO: 1 of 1 tasks shown from $HOME/todo.txt
-
->>> todo.sh replace 1 smell the uppercase Roses
-1: smell the roses
-replaced with
-1: smell the uppercase Roses
-
->>> todo.sh list
-1 smell the uppercase Roses
---
-TODO: 1 of 1 tasks shown from $HOME/todo.txt
-
->>> todo.sh list roses
-1 smell the uppercase Roses
---
-TODO: 1 of 1 tasks shown from $HOME/todo.txt
-
->>> todo.sh add notice the sunflowers
-TODO: 'notice the sunflowers' added on line 2.
-
->>> todo.sh list
-2 notice the sunflowers
-1 smell the uppercase Roses
---
-TODO: 2 of 2 tasks shown from $HOME/todo.txt
-
->>> todo.sh append 1 +flowers @outside
-1: smell the uppercase Roses +flowers @outside
-
->>> todo.sh list
-2 notice the sunflowers
-1 smell the uppercase Roses +flowers @outside
---
-TODO: 2 of 2 tasks shown from $HOME/todo.txt
-
->>> todo.sh add "stop"
-TODO: 'stop' added on line 3.
-
->>> todo.sh list
-2 notice the sunflowers
-1 smell the uppercase Roses +flowers @outside
-3 stop
---
-TODO: 3 of 3 tasks shown from $HOME/todo.txt
-
->>> todo.sh pri B B
-usage: $HOME/bin/todo.sh pri ITEM# PRIORITY
-note: PRIORITY must be anywhere from A to Z.
-=== 1
-
->>> todo.sh pri 1 B
-1: (B) smell the uppercase Roses +flowers @outside
-TODO: 1 prioritized (B).
-
->>> todo.sh list
-[0;32m1 (B) smell the uppercase Roses +flowers @outside[0m
-2 notice the sunflowers
-3 stop
---
-TODO: 3 of 3 tasks shown from $HOME/todo.txt
-
->>> todo.sh -p list
-1 (B) smell the uppercase Roses +flowers @outside
-2 notice the sunflowers
-3 stop
---
-TODO: 3 of 3 tasks shown from $HOME/todo.txt
-
->>> todo.sh pri 2 C
-2: (C) notice the sunflowers
-TODO: 2 prioritized (C).
-
->>> todo.sh -p list
-1 (B) smell the uppercase Roses +flowers @outside
-2 (C) notice the sunflowers
-3 stop
---
-TODO: 3 of 3 tasks shown from $HOME/todo.txt
-
->>> todo.sh pri 2 A
-2: (A) notice the sunflowers
-TODO: 2 prioritized (A).
-
->>> todo.sh -p list
-2 (A) notice the sunflowers
-1 (B) smell the uppercase Roses +flowers @outside
-3 stop
---
-TODO: 3 of 3 tasks shown from $HOME/todo.txt
-
->>> todo.sh pri 2 a
-2: (A) notice the sunflowers
-TODO: 2 prioritized (A).
-
->>> todo.sh -p listpri
-2 (A) notice the sunflowers
-1 (B) smell the uppercase Roses +flowers @outside
---
-TODO: 2 of 3 tasks shown from $HOME/todo.txt
-
->>> todo.sh add "smell the coffee +wakeup"
-TODO: 'smell the coffee +wakeup' added on line 4.
-
 >>> todo.sh -p list
 2 (A) notice the sunflowers
 1 (B) smell the uppercase Roses +flowers @outside
@@ -234,32 +107,15 @@ usage: $HOME/bin/todo.sh append ITEM# "TEXT TO APPEND"
 --
 TODO: 5 of 5 tasks shown from $HOME/todo.txt
 
->>> todo.sh pri 2 C
-2: (C) stop and think
-TODO: 2 prioritized (C).
-
->>> todo.sh replace 10 "hej!"
-10: No such todo.
-=== 1
-
->>> todo.sh -p list
-1 (B) smell the uppercase Roses +flowers @outside
-2 (C) stop and think
-4 make the coffee +wakeup
-3 smell the coffee +wakeup
-5 visit http://example.com
---
-TODO: 5 of 5 tasks shown from $HOME/todo.txt
-
 >>> todo.sh append 10 "hej!"
 10: No such todo.
 === 1
 
 >>> todo.sh -p list
 1 (B) smell the uppercase Roses +flowers @outside
-2 (C) stop and think
 4 make the coffee +wakeup
 3 smell the coffee +wakeup
+2 stop and think
 5 visit http://example.com
 --
 TODO: 5 of 5 tasks shown from $HOME/todo.txt
@@ -270,9 +126,9 @@ TODO: 5 of 5 tasks shown from $HOME/todo.txt
 
 >>> todo.sh -p list
 1 (B) smell the uppercase Roses +flowers @outside
-2 (C) stop and think
 4 make the coffee +wakeup
 3 smell the coffee +wakeup
+2 stop and think
 5 visit http://example.com
 --
 TODO: 5 of 5 tasks shown from $HOME/todo.txt
@@ -282,9 +138,9 @@ TODO: 'the coffee +wakeup' added on line 6.
 
 >>> todo.sh -p list
 1 (B) smell the uppercase Roses +flowers @outside
-2 (C) stop and think
 4 make the coffee +wakeup
 3 smell the coffee +wakeup
+2 stop and think
 6 the coffee +wakeup
 5 visit http://example.com
 --
@@ -295,10 +151,10 @@ TODO: 6 of 6 tasks shown from $HOME/todo.txt
 
 >>> todo.sh -p list
 1 (B) smell the uppercase Roses +flowers @outside
-2 (C) stop and think
 4 make the coffee +wakeup
 6 make the coffee +wakeup
 3 smell the coffee +wakeup
+2 stop and think
 5 visit http://example.com
 --
 TODO: 6 of 6 tasks shown from $HOME/todo.txt
