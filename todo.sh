d2af44d8d3439372dd5731c16b83e9a8e7895317
Ingo Karkat <swdev@ingo-karkat.de>
2012-01-07 22:53:40 +0100

    BUG: SED error when HIDE_..._SUBSTITUTION contains whitespace.
    This error only occurs when add-ons override either HIDE_PROJECTS_SUBSTITUTION or HIDE_CONTEXTS_SUBSTITUTION with a pattern that contains whitespace, not with the values used within todo.sh. But correcting the sloppy quoting doesn't hurt, neither.

--- todo.sh
+++ todo.sh
@@ -812,8 +812,8 @@ _list() {
             }
           '''  \
         | sed '''
-            s/'${HIDE_PROJECTS_SUBSTITUTION:-^}'//g
-            s/'${HIDE_CONTEXTS_SUBSTITUTION:-^}'//g
+            s/'"${HIDE_PROJECTS_SUBSTITUTION:-^}"'//g
+            s/'"${HIDE_CONTEXTS_SUBSTITUTION:-^}"'//g
           '''                                                   \
         | eval ${TODOTXT_FINAL_FILTER}                          \
     )
