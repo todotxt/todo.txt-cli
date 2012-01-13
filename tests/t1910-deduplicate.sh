1160ae1276c43c534cda27c70d6459679e1886f2
Ingo Karkat <swdev@ingo-karkat.de>
2012-01-13 22:44:35 +0100

    Fix deduplicate for non-printable (and non-ASCII) characters.

--- tests/t1910-deduplicate.sh
+++ tests/t1910-deduplicate.sh
@@ -6,7 +6,7 @@ Ensure we can deduplicate items successfully.
 '
 . ./test-lib.sh
 
-cat >> todo.txt<<EOF
+cat > todo.txt <<EOF
 duplicated
 two
 x done
@@ -33,6 +33,26 @@ EOF
 test_todo_session 'deduplicate without duplicates' <<EOF
 >>> todo.sh deduplicate
 TODO: No duplicate tasks found
+EOF
+
+cat > todo.txt <<EOF
+normal task
+a [1mbold[0m action
+something else
+a [1mbold[0m action
+something more
+EOF
+test_todo_session 'deduplicate with non-printable duplicates' <<EOF
+>>> todo.sh deduplicate
+TODO: 1 duplicate task(s) removed
+
+>>> todo.sh -p ls
+2 a [1mbold[0m action
+1 normal task
+3 something else
+4 something more
+--
+TODO: 4 of 4 tasks shown
 EOF
 
 test_done
