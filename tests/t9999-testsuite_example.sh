d46adadb1d1336f50e7cac6a28c64d735fcdd2fe
Ingo Karkat <swdev@ingo-karkat.de>
2012-01-14 15:10:22 +0100

    Align report order with other actions.
    Like "add" or "do", first the object, then (in verbose mode) the status message is printed.

--- tests/t9999-testsuite_example.sh
+++ tests/t9999-testsuite_example.sh
@@ -86,13 +86,8 @@ TODO: 5 of 5 tasks shown
 
 >>> todo.sh report
 TODO: $HOME/todo.txt archived.
-TODO: Report file updated.
-2009-02-13-04:40:00 5 1
-
->>> todo.sh report
-TODO: $HOME/todo.txt archived.
-TODO: Report file updated.
 2009-02-13T04:40:00 5 1
+TODO: Report file updated.
 
 >>> todo.sh append g a
 usage: todo.sh append ITEM# "TEXT TO APPEND"
@@ -162,21 +157,6 @@ TODO: 6 of 6 tasks shown
 5 visit http://example.com
 --
 TODO: 6 of 6 tasks shown
-
->>> todo.sh -a do 3
-3 x 2009-02-13 smell the coffee +wakeup
-TODO: 3 marked as done.
-
->>> todo.sh report
-x 2009-02-13 smell the coffee +wakeup
-TODO: $HOME/todo.txt archived.
-TODO: Report file updated.
-2009-02-13T04:40:00 5 2
-
->>> todo.sh report
-TODO: $HOME/todo.txt archived.
-TODO: Report file updated.
-2009-02-13T04:40:00 5 2
 
 >>> todo.sh remdup
 Usage: todo.sh [-fhpantvV] [-d todo_config] action [task_number] [task_description]
