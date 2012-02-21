9e13dfd2907d92bf6b400afb5edcbd65c4eb3c44
Ingo Karkat <swdev@ingo-karkat.de>
2012-02-21 12:11:45 +0100

    ENH: TODOTXT_SOURCEVAR enables different source file for listcon.
    To be used by todo_completion (to offer candidates from done.txt when there are no matches in todo.txt), and maybe by custom addons.

--- tests/t1310-listcon.sh
+++ tests/t1310-listcon.sh
@@ -55,4 +55,23 @@ test_todo_session 'listcon e-mail address test' <<EOF
 @con02
 EOF
 
+cat > todo.txt <<EOF
+@con01 -- Some context 1 task
+EOF
+cat > done.txt <<EOF
+x 2012-02-21 @done01 -- Some context 1 done task
+x 2012-02-21 @done02 -- Some context 2 done task
+EOF
+test_todo_session 'listcon from done tasks' <<'EOF'
+>>> TODOTXT_SOURCEVAR=\$DONE_FILE todo.sh listcon
+@done01
+@done02
+EOF
+test_todo_session 'listcon from combined open + done tasks' <<'EOF'
+>>> TODOTXT_SOURCEVAR='("$TODO_FILE" "$DONE_FILE")' todo.sh listcon
+@con01
+@done01
+@done02
+EOF
+
 test_done
