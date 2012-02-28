707ebf15221585fd5547640dde9da73dca9ceaa3
Ingo Karkat <swdev@ingo-karkat.de>
2012-02-28 14:55:27 +0100

    ENH: Better completion for custom add-ons via new listaddons command.
    The todo_completion had the problem that it didn't consider the todo.cfg itself for the location of TODO_ACTIONS_DIR, it just tried the default location or a globally exported config value. With the injection of custom configuration now in place, we can actually delegate the listing to todo.sh itself.
    The added built-in "listaddons" command is used for that; it may also be helpful for troubleshooting or to find out about available add-ons. (But the help / shorthelp commands offer more information.)
    
    Additionally, completion is now more precise; only executable actions and no subdirs are listed now; this is also covered by the enhanced test.
    
    The last bonus: The custom add-on actions are now only determined when there's actually completion of commands.

--- tests/t6000-completion.sh
+++ tests/t6000-completion.sh
@@ -7,7 +7,7 @@ This test checks basic todo_completion of actions and options
 '
 . ./test-lib.sh
 
-readonly ACTIONS='add a addto addm append app archive command del rm depri dp do help list ls listall lsa listcon lsc listfile lf listpri lsp listproj lsprj move mv prepend prep pri p replace report shorthelp'
+readonly ACTIONS='add a addto addm append app archive command del rm depri dp do help list ls listaddons listall lsa listcon lsc listfile lf listpri lsp listproj lsprj move mv prepend prep pri p replace report shorthelp'
 readonly OPTIONS='-@ -@@ -+ -++ -d -f -h -p -P -PP -a -n -t -v -vv -V -x'
 
 test_todo_completion 'all arguments' 'todo.sh ' "$ACTIONS $OPTIONS"
