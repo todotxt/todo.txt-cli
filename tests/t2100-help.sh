c0c93c8c0c1849ddb2492910cafb6dad4a3468ad
Ingo Karkat <swdev@ingo-karkat.de>
2012-05-11 09:05:44 +0200

    FIX: Adapt test for OS X.
    POSIX sed doesn't understand the \w atom; use [A-Z] instead; it's also more precise.

--- tests/t2100-help.sh
+++ tests/t2100-help.sh
@@ -11,21 +11,21 @@ This test covers the help output.
 # Note: To avoid having to adapt the test whenever the help documentation
 # slightly changes, only check for the section headers.
 test_todo_session 'help output' <<EOF
->>> todo.sh help | sed '/^  \w/!d'
+>>> todo.sh help | sed '/^  [A-Z]/!d'
   Usage: todo.sh [-fhpantvV] [-d todo_config] action [task_number] [task_description]
   Options:
   Built-in Actions:
 EOF
 
 test_todo_session 'verbose help output' <<EOF
->>> todo.sh -v help | sed '/^  \w/!d'
+>>> todo.sh -v help | sed '/^  [A-Z]/!d'
   Usage: todo.sh [-fhpantvV] [-d todo_config] action [task_number] [task_description]
   Options:
   Built-in Actions:
 EOF
 
 test_todo_session 'very verbose help output' <<EOF
->>> todo.sh -vv help | sed '/^  \w/!d'
+>>> todo.sh -vv help | sed '/^  [A-Z]/!d'
   Usage: todo.sh [-fhpantvV] [-d todo_config] action [task_number] [task_description]
   Options:
   Environment variables:
@@ -34,7 +34,7 @@ EOF
 
 make_action "foo"
 test_todo_session 'help output with custom action' <<EOF
->>> todo.sh -v help | sed '/^  \w/!d'
+>>> todo.sh -v help | sed '/^  [A-Z]/!d'
   Usage: todo.sh [-fhpantvV] [-d todo_config] action [task_number] [task_description]
   Options:
   Built-in Actions:
