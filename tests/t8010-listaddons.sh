3e1ef306dffb24d33628d7032c204870da0f2248
nthorne <notrupertthorne@gmail.com>
2013-11-26 21:21:16 +0100

    Corrected bugs in unit test.
    Executable bit was tested on the wrong file, when
    determining if test was to be skipped. Also, the
    test number in the SKIP_TESTS variable was wrong.

--- tests/t8010-listaddons.sh
+++ tests/t8010-listaddons.sh
@@ -70,8 +70,8 @@ EOF
 chmod -x .todo.actions.d/norris/norris
 # On Cygwin, clearing the executable flag may have no effect, as the Windows ACL
 # may still grant execution rights. In this case, we skip the test.
-if [ -x .todo.actions.d/foo ]; then
-    SKIP_TESTS="${SKIP_TESTS}${SKIP_TESTS+ }t8010.6"
+if [ -x .todo.actions.d/norris/norris ]; then
+    SKIP_TESTS="${SKIP_TESTS}${SKIP_TESTS+ }t8010.8"
 fi
 test_todo_session 'nonexecutable action in subfolder' <<EOF
 >>> todo.sh listaddons
