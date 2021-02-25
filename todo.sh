f5d367f5dc01539580101643e5f91e1c9a8cb49f
Ingo Karkat <swdev@ingo-karkat.de>
2021-02-25 22:00:56 +0100

    Documentation: Correct quoting in todo.sh help list
    Though the double quoting doesn't do harm when the single backslash is followed by the | character (\| is no valid escape sequence), it's technically more correct to use single quotes, and also is more in line with the quoting advice given directly below it.

--- todo.sh
+++ todo.sh
@@ -217,7 +217,7 @@ actionsHelp()
 		      Displays all tasks that contain TERM(s) sorted by priority with line
 		      numbers.  Each task must match all TERM(s) (logical AND); to display
 		      tasks that contain any TERM (logical OR), use
-		      "TERM1\|TERM2\|..." (with quotes), or TERM1\\\|TERM2 (unquoted).
+		      'TERM1\|TERM2\|...' (with quotes), or TERM1\\\|TERM2 (unquoted).
 		      Hides all tasks that contain TERM(s) preceded by a
 		      minus sign (i.e. -TERM).
 		      TERM(s) are grep-style basic regular expressions; for literal matching,
