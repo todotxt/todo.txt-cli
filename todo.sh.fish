47020c051e19e3153c130c902c332223a6fb266c
Pavel Savchenko <asfaltboy@gmail.com>
2021-05-15 20:40:57 +0100

    Add projects completion when + entered

--- todo.sh.fish
+++ todo.sh.fish
@@ -7,6 +7,7 @@
 # Implemented
 #
 # - [x] initial subcommands
+# - [x] project completion for add*
 #
 # Todo
 #
@@ -22,16 +23,32 @@ set args (string split ' ' $all_args | cut -d'|' -f1)
 # get all args help in a lazy manner (fetch and memoize)
 set help_args ""
 function __fish_todosh_args_with_help
-    if test $help_args = ""
-      for arg in (string split ' ' $args);
-          set help (todo.sh help $arg | sed -n -e '/      /,// p' | string trim | tr '\n' ' ')
-          set help_args $help_args(printf '%s\t%s' $arg $help)\n
-      end
+  if test $help_args = ""
+    for arg in (string split ' ' $args);
+      set help (todo.sh help $arg | sed -n -e '/      /,// p' | string trim | tr '\n' ' ')
+        set help_args $help_args(printf '%s\t%s' $arg $help)\n
     end
-    echo $help_args | awk NF
+  end
+  echo $help_args | awk NF
 end
 
 # prevent files completion to todo.sh
 complete -c todo.sh -f
 # add subcommands and prevent further repetition
-complete -c todo.sh -a "(__fish_todosh_args_with_help)" -n "not __fish_seen_subcommand_from $args"
+complete -c todo.sh -n "not __fish_seen_subcommand_from $args" -a "(__fish_todosh_args_with_help)"
+
+# get projects
+function __fish_todosh_plus
+  if __fish_seen_subcommand_from add addm
+    if test (commandline | string split "")[-1] = "+"
+      return 0
+    end
+  end
+  return 1
+end
+function __fish_todosh_projects
+  todo.sh listproj
+end
+
+# add projects to all commands that add text
+complete -c todo.sh -n "__fish_todosh_plus" -a "(__fish_todosh_projects)"
