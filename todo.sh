581de8d1b2bb61a5d93fedb1d8ea0f13b94ba7a8
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-08-10 19:33:46 +0000

    Revert "move bash version check to proper place"
    Breaks on macOS
    
    This reverts commit 17e3e88a78951c6cca676766da4693a024acdab7.

--- todo.sh
+++ todo.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Check bash version
-(( BASH_VERSINFO[0] > 4 || BASH_VERSINFO[0] == 4 && BASH_VERSINFO[1] >= 1 )) || { printf 'Bash 4.1 or newer requried\n' >&2; exit 1; }
-
 # === HEAVY LIFTING ===
 shopt -s extglob extquote
 
@@ -368,8 +365,11 @@ confirm()
     [ "$TODOTXT_FORCE" = 0 ] || return 0
 
     printf %s "${1:?}? (y/n) "
+    local readArgs=(-e -r)
+    [ -n "${BASH_VERSINFO:-}" ] && [ \( ${BASH_VERSINFO[0]} -eq 4 -a ${BASH_VERSINFO[1]} -ge 1 \) -o ${BASH_VERSINFO[0]} -gt 4 ] &&
+        readArgs+=(-N 1)    # Bash 4.1+ supports -N nchars
     local answer
-    read -r -e -N 1 answer
+    read "${readArgs[@]}" answer
     echo
     [ "$answer" = "y" ]
 }
