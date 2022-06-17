466265175bdb119f1dbe1ecb5dd78af37d4528f3
Ingo Karkat <swdev@ingo-karkat.de>
2022-06-17 23:39:42 +0200

    Refactoring: Use read -p MSG instead of doing echo -n MSG separately
    I've seen strange readline editing behavior when the editing doesn't start at the first column: I can actually backspace into the prepended message (with Del, Ctrl-W or Ctrl-U), and then the whole edit becomes messed up.
    
    read can output a prompt on its own (hopefully in all versions of Bash that we aim to support - the tests will tell), and that doesn't have this problem, and it's also a bit cleaner and shorter.
    
    The prompt is only displayed if input is coming from a terminal. For the tests (currently only deletion and move confirmations are covered), this means that the prompt itself cannot be covered, and an empty line instead has to be expected. (On the positive side, this removes the ugly trick with $SPACE.)

--- todo.sh
+++ todo.sh
@@ -364,12 +364,11 @@ confirm()
 {
     [ $TODOTXT_FORCE = 0 ] || return 0
 
-    printf %s "${1:?}? (y/n) "
     local readArgs=(-e -r)
     [ -n "${BASH_VERSINFO:-}" ] && [ \( ${BASH_VERSINFO[0]} -eq 4 -a ${BASH_VERSINFO[1]} -ge 1 \) -o ${BASH_VERSINFO[0]} -gt 4 ] &&
         readArgs+=(-N 1)    # Bash 4.1+ supports -N nchars
     local answer
-    read "${readArgs[@]}" answer
+    read -p "${1:?}? (y/n) " "${readArgs[@]}" answer
     echo
     [ "$answer" = "y" ]
 }
@@ -451,8 +450,7 @@ replaceOrPrepend()
   getTodo "$item"
 
   if [[ -z "$1" && $TODOTXT_FORCE = 0 ]]; then
-    echo -n "$querytext"
-    read -r -i "$todo" -e input
+    read -p "$querytext" -r -i "$todo" -e input
   else
     input=$*
   fi
@@ -1065,8 +1063,7 @@ fi
 case $action in
 "add" | "a")
     if [[ -z "$2" && $TODOTXT_FORCE = 0 ]]; then
-        echo -n "Add: "
-        read -e -r input
+        read -p "Add: " -e -r input
     else
         [ -z "$2" ] && die "usage: $TODO_SH add \"TODO ITEM\""
         shift
@@ -1077,8 +1074,7 @@ case $action in
 
 "addm")
     if [[ -z "$2" && $TODOTXT_FORCE = 0 ]]; then
-        echo -n "Add: "
-        read -e -r input
+        read -p "Add: " -e -r input
     else
         [ -z "$2" ] && die "usage: $TODO_SH addm \"TODO ITEM\""
         shift
@@ -1118,8 +1114,7 @@ case $action in
     getTodo "$item"
 
     if [[ -z "$1" && $TODOTXT_FORCE = 0 ]]; then
-        echo -n "Append: "
-        read -e -r input
+        read -p "Append: " -e -r input
     else
         input=$*
     fi
