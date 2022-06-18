bab2af950186380d6106702768a5a7cce7d84de0
Ingo Karkat <swdev@ingo-karkat.de>
2022-06-18 10:55:04 +0200

    Tests: Refactoring: Extract invalidate_action() into actions-test-lib
    We don't "shamelessly steal" code, we refactoring it ;-)

--- tests/actions-test-lib.sh
+++ tests/actions-test-lib.sh
@@ -34,3 +34,19 @@ echo "custom action $1 in folder $1"
 EOF
 chmod +x ".todo.actions.d/$1/$1"
 }
+
+invalidate_action()
+{
+    local customActionFilespec="${1:?}"; shift
+    local testName="${1:?}"; shift
+
+    chmod -x "$customActionFilespec"
+    # On Cygwin, clearing the executable flag may have no effect, as the Windows
+    # ACL may still grant execution rights. In this case, we skip the test, and
+    # remove the (still valid) custom action so that it doesn't break following
+    # tests.
+    if [ -x "$customActionFilespec" ]; then
+        SKIP_TESTS="${SKIP_TESTS}${SKIP_TESTS+ }${testName}"
+        rm -- "$customActionFilespec"
+    fi
+}
