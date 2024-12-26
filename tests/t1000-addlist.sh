5d2727f4ec3bb5654ede97ab97617f0d6cef301b
Edwin Kofler <edwin@kofler.dev>
2024-12-26 01:54:53 -0800

    fix: Use shebang `#!/usr/bin/env bash` (#418)

--- tests/t1000-addlist.sh
+++ tests/t1000-addlist.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 test_description='basic add and list functionality
 
--- tests/t1030-addto-date.sh
+++ tests/t1030-addto-date.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 test_description='test the date on addto feature
 
