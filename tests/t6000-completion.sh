889ac12c6b7c61d68f2d0db1c4554ddf7d322499
Ingo Karkat <swdev@ingo-karkat.de>
2026-08-07 10:55:44 +0200

    Tests: Refactoring: Extract dupliate ACTIONS and OPTIONS definitions into completion-test-lib

--- tests/t6000-completion.sh
+++ tests/t6000-completion.sh
@@ -4,10 +4,8 @@ test_description='Bash completion functionality
 
 This test checks basic todo_completion of actions and options
 '
+. ./completion-test-lib.sh
 . ./test-lib.sh
-
-readonly ACTIONS='add a addto addm append app archive command del rm deduplicate depri dp do done help list ls listaddons listall lsa listcon lsc listfile lf listpri lsp listproj lsprj move mv prepend prep pri p replace report shorthelp'
-readonly OPTIONS='-@ -@@ -+ -++ -d -f -h -p -P -PP -a -n -t -v -vv -V -x'
 
 test_todo_completion 'all arguments' 'todo.sh ' "$ACTIONS $OPTIONS"
 test_todo_completion 'arguments beginning with a' 'todo.sh a' 'add a addto addm append app archive'
--- tests/t6100-completion-help.sh
+++ tests/t6100-completion-help.sh
@@ -5,11 +5,11 @@ test_description='Bash help completion functionality
 This test checks todo_completion of actions for usage help.
 '
 . ./actions-test-lib.sh
+. ./completion-test-lib.sh
 . ./test-lib.sh
+
 make_action "zany"
 make_action "aardvark"
-
-readonly ACTIONS='add a addto addm append app archive command del rm deduplicate depri dp do done help list ls listaddons listall lsa listcon lsc listfile lf listpri lsp listproj lsprj move mv prepend prep pri p replace report shorthelp'
 readonly ADDONS='aardvark zany'
 
 test_todo_completion 'all actions after help' 'todo.sh help ' "$ACTIONS $ADDONS"
