33694051ed33aa175744818fd207b5d54e16bb79
Ingo Karkat <swdev@ingo-karkat.de>
2026-08-07 10:40:37 +0200

    todo_completion: FIX: Add completion for missing done and deduplicate built-in actions

--- tests/t6100-completion-help.sh
+++ tests/t6100-completion-help.sh
@@ -9,7 +9,7 @@ This test checks todo_completion of actions for usage help.
 make_action "zany"
 make_action "aardvark"
 
-readonly ACTIONS='add a addto addm append app archive command del rm depri dp do help list ls listaddons listall lsa listcon lsc listfile lf listpri lsp listproj lsprj move mv prepend prep pri p replace report shorthelp'
+readonly ACTIONS='add a addto addm append app archive command del rm deduplicate depri dp do done help list ls listaddons listall lsa listcon lsc listfile lf listpri lsp listproj lsprj move mv prepend prep pri p replace report shorthelp'
 readonly ADDONS='aardvark zany'
 
 test_todo_completion 'all actions after help' 'todo.sh help ' "$ACTIONS $ADDONS"
